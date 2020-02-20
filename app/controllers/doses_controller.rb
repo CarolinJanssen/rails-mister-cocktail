45b93c8423353b5e52a71991ed420b346c07da61
Carolin Janssen <c.j@hempberg.com>
2020-02-20 16:25:25 +0100

    models and routes work

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,37 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:cocktail_id, :ingredient_id, :description)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+end
